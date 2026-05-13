78e711fbb977345ed5c0858607147d3f58ac8776
QishuaiLiu <liuqs1216@gmail.com>
2026-05-13 23:31:19 +0800

    init project 7.

---
+++ project_7/src/main.cu
@@ -0,0 +1,28 @@
+#include <cuda_runtime.h>
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+#define CUDA_CHECK(call)                                                          \
+    do {                                                                          \
+        cudaError_t err = (call);                                                 \
+        if (err != cudaSuccess) {                                                 \
+            std::fprintf(stderr, "CUDA error at %s:%d: %s\n", __FILE__, __LINE__, \
+                         cudaGetErrorString(err));                                \
+            std::exit(EXIT_FAILURE);                                              \
+        }                                                                         \
+    } while (0)
+
+__global__ void hello_kernel() {
+    std::printf("Hello from project_7 block %d, thread %d\n", blockIdx.x, threadIdx.x);
+}
+
+int main() {
+    hello_kernel<<<2, 4>>>();
+    CUDA_CHECK(cudaGetLastError());
+    CUDA_CHECK(cudaDeviceSynchronize());
+
+    std::cout << "project_7 initialized successfully" << std::endl;
+    return 0;
+}
