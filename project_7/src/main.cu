d80c4a09c5828365bc1cb896487f213ae30902f7
QishuaiLiu <liuqs1216@gmail.com>
2026-05-14 00:22:33 +0800

    add prepare data for project 7.

--- project_7/src/main.cu
+++ project_7/src/main.cu
@@ -3,6 +3,8 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include <random>
+#include <vector>
 
 #define CUDA_CHECK(call)                                                          \
     do {                                                                          \
@@ -18,10 +20,35 @@ __global__ void hello_kernel() {
     std::printf("Hello from project_7 block %d, thread %d\n", blockIdx.x, threadIdx.x);
 }
 
+__global__ void prefixSum(const float* __restrict__ input, float* result, int size) { return; }
+
 int main() {
     hello_kernel<<<2, 4>>>();
     CUDA_CHECK(cudaGetLastError());
     CUDA_CHECK(cudaDeviceSynchronize());
+
+    int size = 2050;
+    std::vector<float> h_data(size);
+    float result = 0;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    std::uniform_real_distribution<float> dist(0.0f, 10.f);
+
+    for (int i = 0; i < size; ++i) {
+        h_data[i] = dist(gen);
+    }
+
+    float *d_data, *d_result;
+    cudaMalloc(&d_data, size * sizeof(float));
+    cudaMalloc(&d_result, 1 * sizeof(float));
+
+    cudaMemcpy(d_data, h_data.data(), size * sizeof(float), cudaMemcpyHostToDevice);
+
+    dim3 block_dim(256);
+    dim3 grid_dim(size + (block_dim.x - 1) / block_dim.x);
+
+    cudaMemcpy(&result, d_result, sizeof(float), cudaMemcpyDeviceToHost);
 
     std::cout << "project_7 initialized successfully" << std::endl;
     return 0;
